ffdef67abb51457c9c13501ee15f32f2022fe5f4
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-19 22:24:02 +0100

    Added tokenizer unit tests.

--- src/main/java/org/elece/sql/token/processor/NumberTokenProcessor.java
+++ src/main/java/org/elece/sql/token/processor/NumberTokenProcessor.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 /**
- * Only supports integer values. Float values can be parsed by identifying that we have two NumberToken instances between a comma SymbolToken.
+ * Only supports integer values.
  */
 public class NumberTokenProcessor implements ITokenProcessor<Character> {
     @Override
---
+++ src/test/java/org/elece/sql/token/TokenizerTest.java
@@ -0,0 +1,255 @@
+package org.elece.sql.token;
+
+import org.elece.sql.token.error.TokenizerException;
+import org.elece.sql.token.model.*;
+import org.elece.sql.token.model.type.Keyword;
+import org.elece.sql.token.model.type.Symbol;
+import org.elece.sql.token.model.type.Whitespace;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class TokenizerTest {
+    @Test
+    public void test_select() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("SELECT id, name FROM users;");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Select)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.From)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_selectWhere() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("SELECT id, name FROM users where id > 10;");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Select)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.From)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Where)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Gt)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new NumberToken("10")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_selectOrderBy() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("SELECT id, name FROM users ORDER BY name;");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Select)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.From)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Order)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.By)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_selectWhereWithAndOr() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("SELECT id, name FROM users WHERE id > 5 AND id < 10 OR name = 'name';");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Select)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.From)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Where)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Gt)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new NumberToken("5")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.And)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Lt)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new NumberToken("10")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Or)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eq)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new StringToken("name")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_createTable() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("CREATE TABLE users (id INT PRIMARY KEY, name VARCHAR(255));");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Create)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Table)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.LeftParenthesis)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Int)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Primary)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Key)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Varchar)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.LeftParenthesis)).build(),
+                TokenWrapper.builder().token(new NumberToken("255")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.RightParenthesis)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.RightParenthesis)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_updateTable() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("UPDATE users SET name = 'updatedName' WHERE id = 1;");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Update)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Set)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eq)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new StringToken("updatedName")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Where)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eq)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new NumberToken("1")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_deleteFrom() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("DELETE FROM users WHERE id = 1;");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Delete)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.From)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Where)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eq)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new NumberToken("1")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+
+    @Test
+    public void test_insertInto() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("INSERT INTO users (id, name) VALUES (1, 'name');");
+        List<TokenWrapper> actualTokens = tokenizer.tokenize();
+        TokenWrapper[] expectedTokens = new TokenWrapper[]{
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Insert)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Into)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("users")).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.LeftParenthesis)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("id")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new IdentifierToken("name")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.RightParenthesis)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new KeywordToken(Keyword.Values)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.LeftParenthesis)).build(),
+                TokenWrapper.builder().token(new NumberToken("1")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Comma)).build(),
+                TokenWrapper.builder().token(new WhitespaceToken(Whitespace.Space)).build(),
+                TokenWrapper.builder().token(new StringToken("name")).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.RightParenthesis)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.SemiColon)).build(),
+                TokenWrapper.builder().token(new SymbolToken(Symbol.Eof)).build()
+        };
+        Assertions.assertArrayEquals(expectedTokens, actualTokens.toArray(TokenWrapper[]::new));
+    }
+}
