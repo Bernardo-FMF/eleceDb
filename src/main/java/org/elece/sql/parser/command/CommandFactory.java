587c1b0c011bf0c913a32ed229d72c1076b03f89
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-22 21:18:54 +0100

    Created the base statement command factory.

---
+++ src/main/java/org/elece/sql/parser/command/CommandFactory.java
@@ -0,0 +1,14 @@
+package org.elece.sql.parser.command;
+
+import org.elece.sql.token.IPeekableIterator;
+import org.elece.sql.token.TokenWrapper;
+import org.elece.sql.token.model.type.Keyword;
+
+public class CommandFactory {
+    public IKeywordCommand buildCommand(Keyword keyword, IPeekableIterator<TokenWrapper> tokenizer) {
+        return switch (keyword) {
+            case Select -> new SelectKeywordCommand(tokenizer);
+            default -> null;
+        };
+    }
+}
