b5498083790cc1e2f11ca0d55f2cc3811a0c8265
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-24 19:58:24 +0100

    Implemented the commit and rollback keyword command.

---
+++ src/main/java/org/elece/sql/parser/command/SimpleKeywordCommand.java
@@ -0,0 +1,31 @@
+package org.elece.sql.parser.command;
+
+import org.elece.sql.parser.StatementWrapper;
+import org.elece.sql.parser.error.SqlException;
+import org.elece.sql.parser.statement.CommitStatement;
+import org.elece.sql.parser.statement.RollbackStatement;
+import org.elece.sql.parser.statement.Statement;
+import org.elece.sql.token.IPeekableIterator;
+import org.elece.sql.token.TokenWrapper;
+import org.elece.sql.token.error.TokenizerException;
+import org.elece.sql.token.model.type.Keyword;
+
+public class SimpleKeywordCommand extends AbstractIKeywordCommand {
+    private Keyword keyword;
+
+    public SimpleKeywordCommand(IPeekableIterator<TokenWrapper> tokenizer, Keyword keyword) {
+        super(tokenizer);
+        this.keyword = keyword;
+    }
+
+    @Override
+    public StatementWrapper parse() throws SqlException {
+        Statement statement = switch (keyword) {
+            case Commit -> new CommitStatement();
+            case Rollback -> new RollbackStatement();
+            default -> throw new SqlException("");
+        };
+
+        return StatementWrapper.builder().statement(statement).build();
+    }
+}
