e57101fdcb512564b09ab41e5280a2b083fb410b
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-30 23:17:55 +0100

    Implemented the drop table analyzer command.

---
+++ src/main/java/org/elece/sql/analyzer/command/DropTableAnalyzerCommand.java
@@ -0,0 +1,22 @@
+package org.elece.sql.analyzer.command;
+
+import org.elece.sql.analyzer.error.AnalyzerException;
+import org.elece.sql.db.IContext;
+import org.elece.sql.db.TableMetadata;
+import org.elece.sql.parser.statement.DropTableStatement;
+import org.elece.sql.parser.statement.Statement;
+
+import java.util.Objects;
+
+public class DropTableAnalyzerCommand implements IAnalyzerCommand {
+    @Override
+    public void analyze(IContext<String, TableMetadata> context, Statement statement) throws AnalyzerException {
+        DropTableStatement dropTableStatement = (DropTableStatement) statement;
+
+        TableMetadata table = context.findMetadata(dropTableStatement.getTable());
+
+        if (Objects.isNull(table)) {
+            throw new AnalyzerException("");
+        }
+    }
+}
