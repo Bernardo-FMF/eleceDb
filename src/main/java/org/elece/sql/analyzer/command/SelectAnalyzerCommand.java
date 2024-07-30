20d259d7760a401bfea1f9c9ade3f6aefd58b3b3
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-30 23:18:19 +0100

    Implemented the select analyzer command.

---
+++ src/main/java/org/elece/sql/analyzer/command/SelectAnalyzerCommand.java
@@ -0,0 +1,37 @@
+package org.elece.sql.analyzer.command;
+
+import org.elece.sql.analyzer.error.AnalyzerException;
+import org.elece.sql.db.IContext;
+import org.elece.sql.db.TableMetadata;
+import org.elece.sql.parser.expression.Expression;
+import org.elece.sql.parser.expression.WildcardExpression;
+import org.elece.sql.parser.statement.SelectStatement;
+import org.elece.sql.parser.statement.Statement;
+
+import java.util.Objects;
+
+public class SelectAnalyzerCommand implements IAnalyzerCommand {
+    @Override
+    public void analyze(IContext<String, TableMetadata> context, Statement statement) throws AnalyzerException {
+        SelectStatement selectStatement = (SelectStatement) statement;
+
+        TableMetadata table = context.findMetadata(selectStatement.getFrom());
+
+        if (Objects.isNull(table)) {
+            throw new AnalyzerException("");
+        }
+
+        for (Expression column : selectStatement.getColumns()) {
+            if (column instanceof WildcardExpression) {
+                continue;
+            }
+            analyzeExpression(table.getSchema(), null, column);
+        }
+
+        analyzeWhere(table.getSchema(), selectStatement.getWhere());
+
+        for (Expression orderBy : selectStatement.getOrderBy()) {
+            analyzeExpression(table.getSchema(), null, orderBy);
+        }
+    }
+}
