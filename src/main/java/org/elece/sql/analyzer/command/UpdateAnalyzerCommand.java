a7da12ef19721c09b76aa43b4f78ab5998f5161c
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-07-30 23:18:31 +0100

    Implemented the update analyzer command.

---
+++ src/main/java/org/elece/sql/analyzer/command/UpdateAnalyzerCommand.java
@@ -0,0 +1,34 @@
+package org.elece.sql.analyzer.command;
+
+import org.elece.sql.analyzer.error.AnalyzerException;
+import org.elece.sql.db.Db;
+import org.elece.sql.db.IContext;
+import org.elece.sql.db.TableMetadata;
+import org.elece.sql.parser.expression.internal.Assignment;
+import org.elece.sql.parser.statement.Statement;
+import org.elece.sql.parser.statement.UpdateStatement;
+
+import java.util.Objects;
+
+public class UpdateAnalyzerCommand implements IAnalyzerCommand {
+    @Override
+    public void analyze(IContext<String, TableMetadata> context, Statement statement) throws AnalyzerException {
+        UpdateStatement updateStatement = (UpdateStatement) statement;
+
+        TableMetadata table = context.findMetadata(updateStatement.getTable());
+
+        if (Objects.isNull(table)) {
+            throw new AnalyzerException("");
+        }
+
+        if (updateStatement.getTable().equals(Db.META_TABLE)) {
+            throw new AnalyzerException("");
+        }
+
+        for (Assignment assignment : updateStatement.getColumns()) {
+            analyzeAssignment(table, assignment, true);
+        }
+
+        analyzeWhere(table.getSchema(), updateStatement.getWhere());
+    }
+}
