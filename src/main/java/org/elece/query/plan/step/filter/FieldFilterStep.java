63050de0b33d161d844b8970de656a3cd29db5fd
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-11-02 19:41:23 +0000

    Implemented filter steps to validate non-indexed data on fetched objects.

---
+++ src/main/java/org/elece/query/plan/step/filter/FieldFilterStep.java
@@ -0,0 +1,64 @@
+package org.elece.query.plan.step.filter;
+
+import org.elece.db.DbObject;
+import org.elece.db.schema.model.Column;
+import org.elece.db.schema.model.Table;
+import org.elece.exception.serialization.DeserializationException;
+import org.elece.query.comparator.ValueComparator;
+import org.elece.serializer.Serializer;
+import org.elece.serializer.SerializerRegistry;
+import org.elece.sql.parser.expression.internal.SqlBoolValue;
+import org.elece.sql.parser.expression.internal.SqlNumberValue;
+import org.elece.sql.parser.expression.internal.SqlStringValue;
+import org.elece.sql.parser.expression.internal.SqlType;
+import org.elece.utils.SerializationUtils;
+
+import java.util.Optional;
+
+public class FieldFilterStep<V> extends FilterStep {
+    private final SerializerRegistry serializerRegistry;
+
+    private final Table table;
+    private final Column column;
+    private final ValueComparator<V> valueComparator;
+
+    protected FieldFilterStep(Table table, Column column, ValueComparator<V> valueComparator, SerializerRegistry serializerRegistry, Long scanId) {
+        super(scanId);
+
+        this.serializerRegistry = serializerRegistry;
+        this.table = table;
+        this.column = column;
+        this.valueComparator = valueComparator;
+    }
+
+    @Override
+    Optional<DbObject> next(DbObject dbObject) {
+        byte[] valueOfField = SerializationUtils.getValueOfField(table, column, dbObject.getData());
+
+        SqlType.Type sqlType = column.getSqlType().getType();
+
+        boolean comparisonResult = true;
+        try {
+            if (sqlType == SqlType.Type.Int) {
+                Serializer<Integer> serializer = serializerRegistry.getSerializer(sqlType);
+                Integer deserializedValue = serializer.deserialize(valueOfField, column);
+
+                comparisonResult = ((ValueComparator<Integer>) valueComparator).compare(new SqlNumberValue(deserializedValue));
+            } else if (sqlType == SqlType.Type.Bool) {
+                Serializer<Boolean> serializer = serializerRegistry.getSerializer(sqlType);
+                Boolean deserializedValue = serializer.deserialize(valueOfField, column);
+
+                comparisonResult = ((ValueComparator<Boolean>) valueComparator).compare(new SqlBoolValue(deserializedValue));
+            } else if (sqlType == SqlType.Type.Varchar) {
+                Serializer<String> serializer = serializerRegistry.getSerializer(sqlType);
+                String deserializedValue = serializer.deserialize(valueOfField, column);
+
+                comparisonResult = ((ValueComparator<String>) valueComparator).compare(new SqlStringValue(deserializedValue));
+            }
+        } catch (DeserializationException exception) {
+            return Optional.empty();
+        }
+
+        return comparisonResult ? Optional.of(dbObject) : Optional.empty();
+    }
+}
---
+++ src/main/java/org/elece/query/plan/step/filter/FilterStep.java
@@ -0,0 +1,15 @@
+package org.elece.query.plan.step.filter;
+
+import org.elece.db.DbObject;
+
+import java.util.Optional;
+
+public abstract class FilterStep {
+    private final Long scanId;
+
+    protected FilterStep(Long scanId) {
+        this.scanId = scanId;
+    }
+
+    abstract Optional<DbObject> next(DbObject dbObject);
+}
